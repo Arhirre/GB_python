d6d76914fe4f30ec87865b387c87b18c9847cc8d
Arhirre <Arhirre@users.noreply.github.com>
2020-05-09 20:46:28 +0300

    ls1
    DZ for lesson1

---
+++ ls1task3dz1.py
@@ -0,0 +1,10 @@
+# dz - Nikita_Savchenko
+
+numUser = input('введите число: ')
+numA = int(numUser)
+numB = int(numUser + numUser)
+numC = int(numUser + numUser + numUser)
+sumABC = numA + numB + numC
+
+print(sumABC)
+
---
+++ ls1task4dz1.py
@@ -0,0 +1,10 @@
+# dz - Nikita_Savchenko
+
+numA = input('введите число')
+
+x = 0
+for i in numA:
+    while int(i) > x:
+        x = int(i)
+
+print(x)
